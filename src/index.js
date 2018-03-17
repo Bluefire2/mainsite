2a45012f2c57606b17e41dfd027014887b6a7464
Kyrylo Chernyshov <kc875@cornell.edu>
2018-03-17 19:47:09 -0400

    Oops

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
-
+import './index.css';
 import App from './components/App';
 import reducers from './reducers';
+import registerServiceWorker from './registerServiceWorker';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-        <App/>
+        <App />
     </Provider>
-    , document.querySelector('.root'));
+    , document.getElementById('root'));
+registerServiceWorker();
