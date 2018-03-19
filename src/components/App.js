56083e155996127fbb7367491cd922a5d645a9e4
Kyrylo Chernyshov <kc875@cornell.edu>
2018-03-19 10:42:00 -0400

    Move router from index.js to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
+import {Router} from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
 import './App.css';
 import Navbar from '../containers/navbar';
+
+const history = createBrowserHistory();
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Navbar/>
+                <Router history={history}>
+                    <Navbar/>
+                </Router>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -6,18 +6,12 @@ import './index.css';
 import App from './components/App';
 import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
-import {Router} from 'react-router';
-import createBrowserHistory from 'history/createBrowserHistory';
-
-const history = createBrowserHistory();
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-        <Router history={history}>
             <App/>
-        </Router>
     </Provider>
     , document.getElementById('root'));
 registerServiceWorker();
