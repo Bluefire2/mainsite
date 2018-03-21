fdb4e9f6871dcaef20833624f90f0c16a50a2cc8
Kyrylo Chernyshov <kc875@cornell.edu>
2018-03-21 12:15:20 -0400

    Change routing to use TransitionGroup instead - this will let me animate transitions by giving me access to new lifecycle methods.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React, {Component} from 'react';
+import React, {Component, Children} from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
+import {TransitionGroup} from 'react-transition-group';
 import './App.css';
 import Navbar from '../containers/navbar';
 import Index from './index';
 import About from './about';
 import Work from './work';
 import Contact from './contact';
+
+const firstChild = props => {
+    const childrenArray = Children.toArray(props.children);
+    return childrenArray[0] || null;
+};
 
 class App extends Component {
     render() {
@@ -16,10 +22,34 @@ class App extends Component {
                     <div>
                         <Navbar/>
                         <div id="content">
-                            <Route exact path="/" component={Index}/>
-                            <Route path="/about" component={About}/>
-                            <Route path="/work" component={Work}/>
-                            <Route path="/contact" component={Contact}/>
+                            <Route
+                                exact path="/"
+                                children={({ match, ...rest }) => (
+                                    <TransitionGroup component={firstChild}>
+                                        {match && <Index {...rest} />}
+                                    </TransitionGroup>
+                                )}/>
+                            <Route
+                                path="/about"
+                                children={({ match, ...rest }) => (
+                                    <TransitionGroup component={firstChild}>
+                                        {match && <About {...rest} />}
+                                    </TransitionGroup>
+                                )}/>
+                            <Route
+                                path="/work"
+                                children={({ match, ...rest }) => (
+                                    <TransitionGroup component={firstChild}>
+                                        {match && <Work {...rest} />}
+                                    </TransitionGroup>
+                                )}/>
+                            <Route
+                                path="/contact"
+                                children={({ match, ...rest }) => (
+                                    <TransitionGroup component={firstChild}>
+                                        {match && <Contact {...rest} />}
+                                    </TransitionGroup>
+                                )}/>
                         </div>
                     </div>
                 </BrowserRouter>
